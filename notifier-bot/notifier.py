557b4933108f4ca7f599f6fc03d252c5c82c5380
tm <timataran@gmail.com>
2019-12-15 02:39:03 +0200

    create commands on startup

--- notifier-bot/notifier.py
+++ notifier-bot/notifier.py
@@ -3,6 +3,7 @@ from bot.sender import Sender
 from bot.receiver import Receiver
 from connection_monitor import ConnectionMonitor
 from util.event_dispatcher import EventDispatcher
+from bot.command import Help, Subscribe, MyId
 
 logger = factory.get_logger('Notifier starter')
 
@@ -12,6 +13,7 @@ class Notifier:
         self._event_dispatcher = EventDispatcher()
 
     def start(self):
+        self._create_commands()
         connection_monitor = ConnectionMonitor(self._event_dispatcher)
         connection_monitor.start()
         bot_sender = Sender(self._event_dispatcher)
@@ -20,6 +22,11 @@ class Notifier:
 
     def get_dispatcher(self):
         return self._event_dispatcher
+
+    def _create_commands(self):
+        Help(self._event_dispatcher)
+        Subscribe(self._event_dispatcher)
+        MyId(self._event_dispatcher)
 
 
 if __name__ == '__main__':
--- notifier-bot/test/test_notifier.py
+++ notifier-bot/test/test_notifier.py
@@ -4,10 +4,11 @@ from unittest.mock import patch, Mock
 from notifier import Notifier
 
 
+@patch('bot.command.settings')
 class TestNotifierStarter(TestCase):
 
     @patch('notifier.EventDispatcher')
-    def test_create_event_dispatcher_on_init(self, dispatcher_constructor_mock):
+    def test_create_event_dispatcher_on_init(self, dispatcher_constructor_mock, settings):
         dispatcher = Mock()
         dispatcher_constructor_mock.return_value = dispatcher
 
@@ -19,7 +20,7 @@ class TestNotifierStarter(TestCase):
     @patch('notifier.ConnectionMonitor')
     @patch('notifier.Receiver')
     @patch('notifier.Sender')
-    def test_pass_dispatcher_to_components_on_start(self, sender, receiver, connection_monitor, dispatcher):
+    def test_pass_dispatcher_to_components_on_start(self, sender, receiver, connection_monitor, dispatcher, settings):
         event_dispatcher = Mock()
         dispatcher.return_value = event_dispatcher
 
@@ -34,7 +35,7 @@ class TestNotifierStarter(TestCase):
     @patch('notifier.ConnectionMonitor')
     @patch('notifier.Receiver')
     @patch('notifier.Sender')
-    def test_invoke_connection_monitor_start(self, sender, receiver, connection_monitor, dispatcher):
+    def test_invoke_connection_monitor_start(self, sender, receiver, connection_monitor, dispatcher, settings):
         monitor = Mock()
         connection_monitor.return_value = monitor
 
@@ -47,7 +48,7 @@ class TestNotifierStarter(TestCase):
     @patch('notifier.ConnectionMonitor')
     @patch('notifier.Receiver')
     @patch('notifier.Sender')
-    def test_invoke_receiver_start(self, sender, receiver, connection_monitor, dispatcher):
+    def test_invoke_receiver_start(self, sender, receiver, connection_monitor, dispatcher, settings):
         bot_receiver = Mock()
         receiver.return_value = bot_receiver
 
@@ -55,3 +56,26 @@ class TestNotifierStarter(TestCase):
         notifier.start()
 
         bot_receiver.start.assert_called()
+
+    @patch('notifier.EventDispatcher')
+    @patch('notifier.ConnectionMonitor')
+    @patch('notifier.Receiver')
+    @patch('notifier.Sender')
+    @patch('notifier.Help')
+    @patch('notifier.Subscribe')
+    @patch('notifier.MyId')
+    def test_create_commands(self,
+                             cmd_id,
+                             cmd_subscribe,
+                             cmd_help,
+                             sender, receiver, connection_monitor, dispatcher, settings):
+        event_dispatcher = Mock()
+        dispatcher.return_value = event_dispatcher
+
+        notifier = Notifier()
+        notifier.start()
+
+        cmd_help.assert_called_with(event_dispatcher)
+        cmd_id.assert_called_with(event_dispatcher)
+        cmd_subscribe.assert_called_with(event_dispatcher)
+
